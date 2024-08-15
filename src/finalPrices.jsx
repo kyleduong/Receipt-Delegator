264e7c974e90d147116291a1eb65eec0e4282d5e
kduong06 <kduong26@go.pasadena.edu>
2024-08-14 18:35:36 -0700

    frontend

---
+++ src/finalPrices.jsx
@@ -0,0 +1,14 @@
+// get as an input, the list/array of strings that the user checked? or just all of them
+// read every line, then extract the price using the thing
+// add the price to the person in the database
+// need to figure out a way that when they add a name, it adds the person to the database 
+// and I can access their total value, where ill add it to
+
+
+
+
+
+// Second note
+
+// need to uncheck every box once submit peoples' putchased items is clicked
+// also have to stop adding new things if the name is already in the order and just edit existing names/objects
